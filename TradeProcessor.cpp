95c754d8541cc1ecac15af3a44a054daf52d574c
MREV1L007 <110015168+MREV1L007@users.noreply.github.com>
2024-06-13 16:16:01 +0530

    Update TradeProcessor.cpp
    moving the struct and the splitstring function

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -2,46 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
-typedef struct {
-    char SC[256]; // SourceCurrency
-    char DC[256]; // DestibationCurrency
-    float Lots;
-    double Price;
-} TR;
-
-
-char** SplitString(const char* str, char delimiter) {
-    int count = 0;
-    const char* ptr = str;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiter) {
-            count++;
-        }
-    }
-
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int i = 0;
-    ptr = str;
-    char* token = (char*)malloc(strlen(str) + 1);
-    int j = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[j] = '\0';
-            tokens[i++] = strdup(token);
-            j = 0;
-        } else {
-            token[j++] = *ptr;
-        }
-        ptr++;
-    }
-    token[j] = '\0';
-    tokens[i++] = strdup(token);
-    tokens[i] = NULL;
-    free(token);
-    return tokens;
-}
-
+#include "TradeRecord.h"
+#include "Converter.h"
 
 int intGetFromString(const char* str, int* value) {
     char* endptr;
