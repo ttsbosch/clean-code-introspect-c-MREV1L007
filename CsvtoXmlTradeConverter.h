cf771a698483c4e4682f0fbe9b84dba15198af82
MREV1L007 <110015168+MREV1L007@users.noreply.github.com>
2024-06-13 16:23:37 +0530

    Update CsvtoXmlTradeConverter.h

--- CsvtoXmlTradeConverter.h
+++ CsvtoXmlTradeConverter.h
@@ -1,1 +1,10 @@
+#ifndef CSV_TO_XML_TRADE_CONVERTER_H_
+#define CSV_TO_XML_TRADE_CONVERTER_H_
 
+#include "TradeRecord.h"
+#include "Converter.h"
+
+
+
+
+#endif /*end of CSV_TO_XML_TRADE_CONVERTER_H_*/
